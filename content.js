6338f314010740d73b9a9cf613ccaa24e9441f80
Siddharth Saurav <sid.saurav11@gmail.com>
2024-09-17 20:48:20 +0530

    remove text decoration

--- content.js
+++ content.js
@@ -212,7 +212,6 @@ function showDetailedInfo(event) {
       "Font Variant": computedStyle.fontVariant,
       "Letter Spacing": computedStyle.letterSpacing,
       "Text Transform": computedStyle.textTransform,
-      "Text Decoration": rgbToHex(computedStyle.textDecoration),
     };
 
     const cssProperties = {
